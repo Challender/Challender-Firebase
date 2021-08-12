07d80fa654c91f1167a26d82645e72e0c9f4b82a
Kim, GiBeom <gibum1228@naver.com>
2021-08-13 07:53:01 +0900

    1.png 이미지 파일을 파이어베이스에 저장

--- Challender/ContentView.swift
+++ Challender/ContentView.swift
@@ -6,11 +6,48 @@
 //
 
 import SwiftUI
+import FirebaseStorage
 
 struct ContentView: View {
+    @ObservedObject private var viewModel = userViewModel()
+    let storage = Storage.storage() // Storage 인스턴스
+    
+    func uploadImage(img:UIImage){
+        var data = Data()
+        data = img.jpegData(compressionQuality: 0.8)! // 이미지를 jpegData로 변경
+        let filepath = "password" // 저장 경로
+        let metaData = StorageMetadata() // 저장 될 파일 유형
+        metaData.contentType = "image/png"
+        storage.reference().child(filepath).putData(data, metadata: metaData){ // 데이터 넣기
+            (metaData, error) in if let error = error {
+                print(error.localizedDescription)
+                return
+            }else{
+                print("성공")
+            }
+        }
+    }
+    
+    func buttonEvent(){
+        let image = UIImage(named: "1.png")
+        uploadImage(img: image!)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Button(action: buttonEvent){
+                Text("버튼")
+            }
+            List(viewModel.users) { user in
+                VStack(alignment: .leading) { // 행
+                    Text(user.name).font(.title)
+                    Text(user.surname).font(.subheadline)
+                }
+            }.navigationBarTitle("Users")
+            .onAppear() {
+                self.viewModel.fetchData()
+            }
+        }
     }
 }
 
